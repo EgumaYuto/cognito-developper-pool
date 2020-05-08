49cc79907bcd34e77fb9eb95467b8dc9a84caac3
cabos <yuto.eguma@gmail.com>
2020-05-08 09:14:06 +0900

    confirm account

--- client/user/main.ts
+++ client/user/main.ts
@@ -1,1 +1,32 @@
-console.log('user');
+import * as AWS from 'aws-sdk';
+import * as ARGV from 'argv';
+
+import UserPool from './resource/temp_files/user_pool.json';
+import Data from './resource/temp_files/user_data.json';
+
+const init = () => {
+    AWS.config.region = 'ap-northeast-1';
+};
+
+const args = ARGV.option({
+    name: 'confirmation-code',
+    short: 'c',
+    type: 'string',
+    description: 'confirmation code'
+}).run();
+
+const getCognitoIdentitySeriviceProvider = () => {
+    return new AWS.CognitoIdentityServiceProvider();
+};
+
+const main = async () => {
+    init();
+    const provider = getCognitoIdentitySeriviceProvider();
+    await provider.confirmSignUp({
+        ClientId: UserPool.app_client_id.value,
+        Username: Data.email,
+        ConfirmationCode: args.options['confirmation-code']
+    }).promise();
+};
+
+main();
--- infra/resource_publisher.sh
+++ infra/resource_publisher.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cat user_pool/terraform.tfstate | jq '.outputs' > ../client/admin/resource/temp_files/user_pool.json
+cat user_pool/terraform.tfstate | jq '.outputs' > ../client/user/resource/temp_files/user_pool.json
 cat id_pool/terraform.tfstate | jq '.outputs' > ../client/user/resource/temp_files/id_pool.json
